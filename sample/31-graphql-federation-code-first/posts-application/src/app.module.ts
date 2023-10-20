7e750f93d5b595793121127550a015bb6f8e4165
Emran Max <xmanemran@gmail.com>
2023-10-20 12:05:52 +0200

    chore: Add auth guard

--- sample/31-graphql-federation-code-first/posts-application/src/app.module.ts
+++ sample/31-graphql-federation-code-first/posts-application/src/app.module.ts
@@ -2,10 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
+import { AuthGuard } from './guard/auth.guard';
 
 @Module({
   imports: [PostsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: 'APP_GUARD',
+      useClass: AuthGuard,
+    },
+  ],
 })
 export class AppModule {}
